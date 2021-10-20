dd8e8da4e118406de4f6942f4bb601ead59a3e57
Antonio Mateus de Sousa <amateus@mcity140.aquarius.cpqd.com.br>
2021-10-20 16:01:36 -0300

    Fixing bugs

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn app:server  0.0.0.0 8050
+#web: gunicorn app:server  0.0.0.0 8050
+web: python app.py
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dashboard import app
 
 if __name__ == '__main__':
-    # app.run_server('0.0.0.0', 8050)
-    server = app.server
+    app.run_server('0.0.0.0', 8050)
+    # server = app.server
