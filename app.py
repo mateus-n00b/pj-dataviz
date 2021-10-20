35dbc65918bf76d370d3b6ed75b74a348900680a
Antonio Mateus de Sousa <amateus@mcity140.aquarius.cpqd.com.br>
2021-10-20 14:12:16 -0300

    Initial

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from dashboard import app
 
 if __name__ == '__main__':
-    app.run_server('0.0.0.0', 8050)
+    # app.run_server('0.0.0.0', 8050)
+    server = app.server
+    server.run('0.0.0.0', 8050)
